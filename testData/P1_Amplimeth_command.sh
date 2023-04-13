abd4f4a397aa4ddd021627d7f2100cfdb6d75ca4
fravaioli <129501206+fravaioli@users.noreply.github.com>
2023-04-13 10:58:47 +0200

    Update P1_Amplimeth_command.sh

--- testData/P1_Amplimeth_command.sh
+++ testData/P1_Amplimeth_command.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-mkdir ~/Desktop/RiboDown_NoDust/P1/P1_AMPLIMETH/FlunkrDNA1
-cd ~/Desktop/RiboDown_NoDust/P1/P1_AMPLIMETH/FlunkrDNA1/
-python ~/Dropbox/Sequencing/Meth_Seq/EpiAlleles/AmpliMethProfiler/ampliMethProfiler.py --refFile ~/Desktop/RiboDown_NoDust/P1/P1_ReferenceSeq/FlunkrDNA1_ReferenceSeq.fasta --primFile ~/Desktop/RiboDown_NoDust/P1/P1_PrimFile/FlunkrDNA1_PrimFile.csv --blastExecPath /Users/MGiulia/anaconda2/envs/AmpliMethProfilerEnv/bin --fastaDir ~/Desktop/RiboDown_NoDust/P1/P1_FASTA --perfQual --dust no --metaFile ~/Desktop/RiboDown_NoDust/P1/Samplesheet_RiboDown_P1.txt --qualDir ~/Desktop/RiboDown_NoDust/P1/P1_AMPLIMETH/FlunkrDNA1/Stat --threshLen 0.2 --nThread 10
-mkdir ~/Desktop/RiboDown_NoDust/P1/P1_AMPLIMETH/FlunkrDNA2
-cd ~/Desktop/RiboDown_NoDust/P1/P1_AMPLIMETH/FlunkrDNA2/
-python ~/Dropbox/Sequencing/Meth_Seq/EpiAlleles/AmpliMethProfiler/ampliMethProfiler.py --refFile ~/Desktop/RiboDown_NoDust/P1/P1_ReferenceSeq/FlunkrDNA2_ReferenceSeq.fasta --primFile ~/Desktop/RiboDown_NoDust/P1/P1_PrimFile/FlunkrDNA2_PrimFile.csv --blastExecPath /Users/MGiulia/anaconda2/envs/AmpliMethProfilerEnv/bin --fastaDir ~/Desktop/RiboDown_NoDust/P1/P1_FASTA --perfQual --dust no --metaFile ~/Desktop/RiboDown_NoDust/P1/Samplesheet_RiboDown_P1.txt --qualDir ~/Desktop/RiboDown_NoDust/P1/P1_AMPLIMETH/FlunkrDNA2/Stat --threshLen 0.2 --nThread 10
+mkdir $PWD/RiboProm1
+cd $PWD/RiboProm1
+python $PWD/ampliMethProfiler.py --refFile $PWD/RiboProm1_ReferenceSeq.fasta --primFile $PWD/RiboProm1_PrimFile.csv --blastExecPath /home/user/anaconda2/envs/AmpliMethProfilerEnv/bin --fastaDir $PWD/fasta --perfQual --dust no --metaFile $PWD/Samplesheet.txt --qualDir $PWD/RiboProm1/Stat --threshLen 0.2 --nThread 10
